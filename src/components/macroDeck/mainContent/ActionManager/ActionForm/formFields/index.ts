d87ebbc31f8a518e8e5bcbc33e3b504c11716a45
Casana E. Bey <info@expresslayouts.com>
2022-10-09 18:51:19 -0400

    Remove old action manager

--- src/components/macroDeck/mainContent/ActionManager/ActionForm/formFields/index.ts
+++
@@ -1,7 +0,0 @@
-/* istanbul ignore file */
-export { default as FormFieldFile } from "./fileField/fileField";
-export { default as FormFieldKey } from "./keyField/keyField";
-export { default as FormFieldNumbers } from "./numberField/numberField";
-export { default as FormFieldSelect } from "./selectField/selectField";
-export { default as FormFieldTextArea } from "./textArea/textArea";
-export { default as FormFieldText } from "./textField/textField";
--- src/components/macroDeck/mainContent/ActionManager/ActionManager.styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const ActionManagerWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 5px;
-`;
